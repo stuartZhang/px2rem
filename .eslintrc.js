b9786d5f6610545853e35b41daffe2b4e3a42c3a
stuart_zhang <stuartpage@qq.com>
2020-11-29 14:33:00 +0800

    linter 格式化。

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,10 @@
 module.exports = {
     extends: [
         'minxing/eslint-config-node8x.js'
-    ]
+    ],
+    rules: {
+        'no-param-reassign': 'off',
+        'no-console': 'off',
+        'no-sync': 'off'
+    }
 };
